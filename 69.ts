e2f89cccb0f3417606fbe64a58e96862bf8e0752
Dichell <9792910@gmail.com>
2023-09-29 20:34:31 +0300

    69

---
+++ 69.ts
@@ -0,0 +1,43 @@
+function mySqrt(x: number): number {
+
+    let start = 0,
+        end = x
+
+    while (start <= end){
+        const avg = Math.floor((start + end) / 2)
+
+        const square = avg ** 2
+
+        if(square <= x){
+            start = avg + 1
+        } else {
+            end = avg - 1
+        }
+    }   
+    return end
+};
+
+
+
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+// Example 1:
+
+// Input: x = 4
+// Output: 2
+// Explanation: The square root of 4 is 2, so we return 2.
+// Example 2:
+
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
+
+// Constraints:
+
+// 0 <= x <= 231 - 1
